cbc884042f778dc46c7022d04627f1c6234187c5
APorechniy <porechniy@list.ru>
2026-04-21 03:57:07 +0500

    Supported HTML attributes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="ru" suppressHydrationWarning data-theme="dark">
+        <html lang="ru" suppressHydrationWarning>
             <body>
                 {children}
             </body>
