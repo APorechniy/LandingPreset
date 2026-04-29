41f19ea5fa0f101391752eb466b25e9c7c4cf970
APorechniy <porechniy@list.ru>
2026-04-30 01:49:11 +0500

    Migrate to CSS

--- scripts/clear.js
+++ scripts/clear.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const sectionsPath = 'src/sections/';
 const pagePath = 'src/app/page.tsx';
 const globalCssPath = 'src/app/global.css';
+const layoutPath = 'src/app/layout.tsx';
 
 try {
     fs.rmSync(sectionsPath, { recursive: true, force: true });
@@ -12,6 +13,9 @@ try {
 
     fs.rmSync(globalCssPath, { recursive: true, force: true });
     console.log('✅ Global styles has been removed');
+
+    fs.rmSync(layoutPath, { recursive: true, force: true });
+    console.log('✅ Layout has been removed');
 } catch (err) {
     console.error('❌ Error while removing:', err);
 }
